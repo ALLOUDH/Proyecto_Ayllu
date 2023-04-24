37237f4177a4af643e953857fb71aeb70011acaa
Layoned <75380573@continental.edu.pe>
2023-04-24 13:10:47 -0500

    creacion de splash,login,frag_cate_publi,profile_user,aparecerlogo,bannerbackground,buttonbackground y edtittext_background

--- app/src/main/java/ay/llu/project_ayllu/Login.java
+++ app/src/main/java/ay/llu/project_ayllu/Login.java
@@ -2,13 +2,47 @@ package ay.llu.project_ayllu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+        EditText edtName, edtPass;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+        TextView txtOlvidaste;
+
+        Button btnIniciar;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+            edtName=findViewById(R.id.edtUserName);
+            edtPass=findViewById(R.id.edtPassword);
+            txtOlvidaste=findViewById(R.id.txtOlvidaste);
+            btnIniciar=findViewById(R.id.btnIniciar);
+        }
+
+        public void IniciarSesion(View view) {
+            String usuario = edtName.getText().toString();
+            String password = edtPass.getText().toString();
+            boolean verificado = false;
+
+            if (usuario.equals("layoned") && password.equals("123456")) {
+                System.out.println("Bienvenido");
+                mostrarMenu();
+
+            } else {
+                edtPass.setError("contraseña incorrecto");
+                edtName.setError("Nombre de Usuario incorrecto");
+            }
+        }
+
+        private void mostrarMenu() {
+            Intent a = new Intent(this, MainActivity.class);
+            startActivity(a);
+        }
 }
---
+++ app/src/main/java/ay/llu/project_ayllu/Splash.java
@@ -0,0 +1,64 @@
+package ay.llu.project_ayllu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView logo;
+
+    Animator animador;
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.imglogo);
+        Animation animacion;
+        animacion = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.aparecerlogo);
+        logo.startAnimation(animacion);
+        rotacion(logo,2500);
+        logo.setOnClickListener(this);
+        esperarYcerrar(3000);
+    }
+
+    private void esperarYcerrar(int tiempo) {
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                llamar();
+            }
+        },tiempo);
+    }
+    public void llamar(){
+        Intent intent = new Intent(Splash.this, Login.class);
+        startActivity(intent);
+    }
+
+    private void rotacion(View view, int dur) {
+        animador = ObjectAnimator.ofFloat(view, "rotation", 0, 360);
+        animador.setDuration(dur);
+        //la animacion se ejecuta al mismo tiempo
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animador);
+        animatorSet.start();
+    }
+
+    @Override
+    public void onClick(View view) {
+        rotacion(logo,3000);
+    }
+}
