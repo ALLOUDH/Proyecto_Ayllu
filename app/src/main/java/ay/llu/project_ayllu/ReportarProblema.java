5e6415a4c97ed8ce7167972c444d247d626ded53
Jesus0107J <103460797+Jesus0107J@users.noreply.github.com>
2023-04-24 14:37:22 -0500

    ReportarProblema

---
+++ app/src/main/java/ay/llu/project_ayllu/ReportarProblema.java
@@ -0,0 +1,14 @@
+package ay.llu.project_ayllu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportarProblema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportar_problema);
+    }
+}
