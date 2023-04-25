0a759731677d6bda88a90cf47bc5380dc4900cf6
Jesus0107J <103460797+Jesus0107J@users.noreply.github.com>
2023-04-24 20:38:19 -0500

    RegistrarProblema 1.0

---
+++ app/src/main/java/ay/llu/project_ayllu/RegistrarProblema.java
@@ -0,0 +1,14 @@
+package ay.llu.project_ayllu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarProblema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_problema);
+    }
+}
