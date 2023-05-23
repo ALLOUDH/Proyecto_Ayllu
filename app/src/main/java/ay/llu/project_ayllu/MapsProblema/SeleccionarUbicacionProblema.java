d902a2206b3b4602b79228361b67c5f69e6f3bf9
Jesus0107J <103460797+Jesus0107J@users.noreply.github.com>
2023-05-22 19:47:23 -0500

    Avance 22-05-2023

--- app/src/main/java/ay/llu/project_ayllu/MapsProblema/SeleccionarUbicacionProblema.java
+++ app/src/main/java/ay/llu/project_ayllu/MapsProblema/SeleccionarUbicacionProblema.java
@@ -80,7 +80,6 @@ public class SeleccionarUbicacionProblema extends FragmentActivity implements On
                         .anchor(0.5F,0.5F));
                 mLat = latLng.latitude;
                 mLong = latLng.longitude;
-                Toast.makeText(SeleccionarUbicacionProblema.this, mLat + "<>" + mLong, Toast.LENGTH_SHORT).show();
                 Intent intent= new Intent(SeleccionarUbicacionProblema.this, RegistrarProblema.class);
                 Bundle punto = new Bundle();
                 punto.putParcelable("lugar", new LatLng(mLat, mLong));
