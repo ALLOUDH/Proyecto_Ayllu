3db6563656300f1129970b42e217057c286133fa
Hector Chavez <48160099+ALLOUDH@users.noreply.github.com>
2023-06-05 00:25:19 -0500

    Semana 05/06/2023 - Correcciones e implementacion

--- app/src/main/java/ay/llu/project_ayllu/ReportarProblema.java
+++ app/src/main/java/ay/llu/project_ayllu/ReportarProblema.java
@@ -2,13 +2,56 @@ package ay.llu.project_ayllu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ReportarProblema extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ReportarProblema extends AppCompatActivity implements View.OnClickListener {
+    EditText edt_descr;
+    Button btn_enviar;
+    ImageView btn_wsp;
+    FirebaseDatabase database;
+    DatabaseReference dbreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportar_problema);
+        edt_descr = findViewById(R.id.edtReportarProblema);
+        btn_enviar = findViewById(R.id.btnEnviar);
+        btn_wsp = findViewById(R.id.btn_wsp);
+        btn_enviar.setOnClickListener(this);
+        dbreference = FirebaseDatabase.getInstance().getReference();
+        edt_descr.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public void onClick(View view) {
+            registrarReporteProblema();
+    }
+
+    private void registrarReporteProblema() {
+        String id = dbreference.push().getKey();
+        String reporte_problema = edt_descr.getText().toString();
+        if (reporte_problema.isEmpty()){
+            Toast.makeText(ReportarProblema.this, "Ingrese su reporte", Toast.LENGTH_SHORT).show();
+        }
+        ReportarClass reportes = new ReportarClass(id,reporte_problema);
+        dbreference.child("Reportes_Problemas").child(id).setValue(reportes);
+        Toast.makeText(ReportarProblema.this, "Problema registrado exitosamente!",Toast.LENGTH_SHORT).show();
+        edt_descr.setText("");
+    }
+
+    public void abrir_wsp(View view) {
     }
 }
--- app/src/main/java/ay/llu/project_ayllu/SignUp.java
+++ app/src/main/java/ay/llu/project_ayllu/SignUp.java
@@ -1,43 +1,162 @@
 package ay.llu.project_ayllu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
     Spinner spinCarreras;
-    EditText edtNombreSU,edtApellidoSU,edtCorreoSU,edtContraSU,edtConfirmPassSU;
+    EditText edtNombreSU,edtApellidoSU,edtCorreoSU,edtContraSU,edtConfirmPassSU,edtNumeroCel;
+    TextView txtCarreraProf;
     Button btnRegistrar;
+    DatabaseReference Ayllu;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
         spinCarreras = findViewById(R.id.Spinner_SignUp);
         edtNombreSU = findViewById(R.id.edt_Nombres_SignUp);
         edtApellidoSU = findViewById(R.id.edt_Apellidos_SignUp);
+        edtNumeroCel = findViewById(R.id.edt_NroCelular_SignUp);
         edtCorreoSU = findViewById(R.id.edt_Email_SignUp);
         edtContraSU = findViewById(R.id.edt_Pass_SignUp);
         edtConfirmPassSU = findViewById(R.id.edt_ConfirmPass_SignUp);
+        txtCarreraProf = findViewById(R.id.txtCarreraPro_SignUp);
         btnRegistrar = findViewById(R.id.btn_Registrar_SignUp);
         btnRegistrar.setOnClickListener(this);
-        Spinner_Signup();
+        Ayllu = FirebaseDatabase.getInstance().getReference();
+        listar_carreras();
     }
-    private void Spinner_Signup() {
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Carreras_Profesionales, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinCarreras.setAdapter(adapter);
+    public void listar_carreras(){
+        final List<Carreras> carreras = new ArrayList<>();
+        Ayllu.child("Carreras_Profesionales").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for (DataSnapshot ds: snapshot.getChildren()){
+                        String id = ds.getKey();
+                        String nombre = ds.child("nombre").getValue().toString();
+                        carreras.add(new Carreras(id, nombre));
+                    }
+                    ArrayAdapter<Carreras> arrayAdapter = new ArrayAdapter<>(SignUp.this, android.R.layout.simple_dropdown_item_1line, carreras);
+                    spinCarreras.setAdapter(arrayAdapter);
+                    spinCarreras.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            String CarreraSelect = adapterView.getItemAtPosition(i).toString();
+                            txtCarreraProf.setText(CarreraSelect);
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 
     @Override
     public void onClick(View view) {
-        Intent call_login = new Intent(this, Login.class);
-        startActivity(call_login);
+        String id = auth.getCurrentUser().getUid();
+        String nombreUser = edtNombreSU.getText().toString().trim();
+        String apellidoUser = edtApellidoSU.getText().toString().trim();
+        String numeroCeluUser = edtNumeroCel.getText().toString().trim();
+        String carreraUser = txtCarreraProf.getText().toString().trim();
+        String correoUser = edtCorreoSU.getText().toString().trim();
+        String contraUser = edtContraSU.getText().toString().trim();
+        String confcontraUser = edtConfirmPassSU.getText().toString().trim();
+
+        if (nombreUser.isEmpty()){
+            edtNombreSU.setError("Ingrese su nombre");
+        }
+        if (apellidoUser.isEmpty()){
+            edtApellidoSU.setError("Ingrese su Apellido");
+        }
+        if (numeroCeluUser.isEmpty()){
+            edtNumeroCel.setError("Ingrese su Nro. de Celular");
+        }
+        if (correoUser.isEmpty()){
+            edtCorreoSU.setError("Ingrese su correo");
+        }
+        if (contraUser.isEmpty()){
+            edtContraSU.setError("Ingrese su contraseña");
+        }
+        if (confcontraUser.isEmpty()){
+            edtConfirmPassSU.setError("Confirme su contraseña");
+        }
+        if (nombreUser.isEmpty() || apellidoUser.isEmpty() || numeroCeluUser.isEmpty() || correoUser.isEmpty() || contraUser.isEmpty() || confcontraUser.isEmpty()){
+            Toast.makeText(this, "Usted necesita rellenar todos los campos", Toast.LENGTH_SHORT).show();
+        }else{
+            if (confcontraUser.equals(contraUser)){
+                registrarUsuario(id,nombreUser,apellidoUser,correoUser,numeroCeluUser,carreraUser,contraUser);
+            }else{
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void registrarUsuario(String id, String  nombreUser, String apellidoUser, String correoUser, String numeroCeluUser,String carreraUser,String contraUser) {
+        auth.createUserWithEmailAndPassword(correoUser,contraUser).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Usuario usuario = new Usuario(id,nombreUser,apellidoUser,correoUser,numeroCeluUser,carreraUser);
+                    Ayllu.child("Usuarios").child(id).setValue(usuario);
+                    Toast.makeText(SignUp.this,"Se ha creado su cuenta exitosamente!", Toast.LENGTH_SHORT).show();
+                    Intent call_login = new Intent(SignUp.this, Login.class);
+                    startActivity(call_login);
+                }else {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(SignUp.this,"El usuario ya existe", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(SignUp.this,"Lo sentimos, no pudimos registrar su cuenta", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
